3d36725acad18c7a1891e9eca4ac95cadbf9deef
dfall33 <david.c.fall33@gmail.com>
2025-02-14 09:17:35 -0500

    begin hcsr04 migration

--- src/main.c
+++ src/main.c
@@ -4,48 +4,313 @@
 #include <stdio.h>
 
 void nano_wait(int);
+void micro_wait(int);
 
-//=============================================================================
-// Part 1: 7-segment display update with DMA
-//=============================================================================
+void spi_cmd(unsigned int);
+void spi_clear();
+void spi_data(unsigned int);
+void spi1_init_oled();
+void spi1_display1(const char *);
+void spi1_display2(const char *);
+void init_spi1();
+void init_gpio();
+void init_exti();
+void setup_tim14();
+void read_hcsr04();
 
-// 16-bits per digit.
-// The most significant 8 bits are the digit number.
-// The least significant 8 bits are the segments to illuminate.
-// uint16_t msg[8] = {0x0000, 0x0100, 0x0200, 0x0300, 0x0400, 0x0500, 0x0600, 0x0700};
-// extern const char font[];
-// // Print an 8-character string on the 8 digits
-// void print(const char str[]);
-// // Print a floating-point value.
-// void printfloat(float f);
+/* ----- Global Variables ----- */
 
-//============================================================================
-// setup_dma() + enable_dma()
+#define FALSE 0
+#define TRUE 1
+volatile int ultrasonic_timed_out = FALSE;
+volatile int ultrasonic_detected = FALSE;
 
-//============================================================================
-// init_tim15()
-//============================================================================
-// Part 2: Debounced keypad scanning.
-//=============================================================================
-// demonstrate get_key_event()
+void init_gpio()
+{
+    // enable clock to GPIOA
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-//============================================================================
-// Varables for boxcar averaging.
-//============================================================================
-//============================================================================
-// All the things you need to test your subroutines.
-//============================================================================
+    // configure PA0 to be in output mode
+    GPIOA->MODER &= ~(GPIO_MODER_MODER0); // reset it
+    GPIOA->MODER |= GPIO_MODER_MODER0_0;  // set it to output mode
+
+    // configure PA1 to be in input mode
+    GPIOA->MODER &= ~(GPIO_MODER_MODER1); // reset it
+}
+
+void init_exti()
+{
+    // enable clock to SYSCFG
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+    // configure PA1 to be an EXTI line
+    SYSCFG->EXTICR[0] &= ~(SYSCFG_EXTICR1_EXTI1); // reset it
+    SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI1_PA; // set it to PA1
+
+    // enable the interrupt on EXTI1
+    EXTI->IMR |= EXTI_IMR_IM1;
+
+    // configure the trigger to be on the rising edge
+    EXTI->RTSR |= EXTI_RTSR_TR1;
+
+    // enable the interrupt in the NVIC
+    NVIC_EnableIRQ(EXTI0_1_IRQn);
+}
+
+void EXTI0_1_IRQHandler()
+{
+    // clear the interrupt flag
+    EXTI->PR |= EXTI_PR_PR1;
+
+    // set the ultrasonic_detected flag
+    ultrasonic_detected = TRUE;
+}
+
+int us_to_cm(int us)
+{
+    return us / 58;
+}
+
+void setup_tim14()
+{
+
+    // enable clock to TIM14
+    RCC->APB1ENR |= RCC_APB1ENR_TIM14EN;
+
+    // TIM14->ARR = 1440000;
+    TIM14->PSC = 48 - 1; // 48 MHz / 48 = 1 MHz
+    TIM14->ARR = 30000;  // 1 MHz / 30000 = 33.33 Hz
+
+    // enable the interrupt on timer overflow
+    TIM14->DIER |= TIM_DIER_UIE;
+
+    // enable the interrupt in the NVIC
+    NVIC_EnableIRQ(TIM14_IRQn);
+
+    // enable the timer
+    // TIM14->CR1 |= TIM_CR1_CEN;
+
+    // don't enable the timer yet, because we want to start it when we start the ultrasonic sensor
+}
+
+void TIM14_IRQHandler()
+{
+    // clear the interrupt flag
+    TIM14->SR &= ~TIM_SR_UIF;
+
+    // time out the ultrasonic sensor
+    ultrasonic_timed_out = TRUE;
+}
+
+void read_hcsr04()
+{
+    // set PA0 to high
+    GPIOA->ODR |= GPIO_ODR_0;
+
+    // hold for 40 microseconds
+    micro_wait(40);
+
+    // set PA0 to low
+    GPIOA->ODR &= ~GPIO_ODR_0;
+
+    // make sure flags are reset
+    ultrasonic_timed_out = FALSE;
+    ultrasonic_detected = FALSE;
+
+    spi_clear();
+    spi1_display1("reading");
+
+    // enable the timer
+    TIM14->CNT = 0;
+    TIM14->CR1 |= TIM_CR1_CEN;
+
+    spi1_display2("reading");
+
+    // wait for the ultrasonic sensor to time out
+    while (!ultrasonic_timed_out && !ultrasonic_detected)
+        ;
+
+    // disable the timer
+    spi_clear();
+    spi1_display1("done");
+
+    // if the ultrasonic sensor timed out, print "timed out"
+    if (ultrasonic_timed_out)
+    {
+        // printf("timed out\n");
+        spi_clear();
+        spi1_display1("timed out");
+    }
+
+    // if the ultrasonic sensor detected something, print the distance
+    if (ultrasonic_detected)
+    {
+        int distance = us_to_cm(TIM14->CNT);
+        spi_clear();
+
+        // create a string to hold the distance
+        char distance_str[16];
+        snprintf(distance_str, 16, "%d", distance);
+
+        // spi1_display1(distance_str);
+        spi1_display2(distance_str);
+    }
+}
+
+void init_spi1()
+{
+    // enable clock for SPI1
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    GPIOA->MODER &= ~(GPIO_MODER_MODER15 | GPIO_MODER_MODER5 | GPIO_MODER_MODER7);
+    GPIOA->MODER |= GPIO_MODER_MODER15_1 | GPIO_MODER_MODER5_1 | GPIO_MODER_MODER7_1;
+
+    // set the alternate function for the pins (SCK, MOSI, NSS are all alternate function 0)
+
+    // reset the alternate function bits for the pins
+    GPIOA->AFR[0] &= ~(GPIO_AFRL_AFRL7 | GPIO_AFRL_AFRL5);
+    GPIOA->AFR[1] &= ~(GPIO_AFRH_AFRH7);
+
+    // configure NSS, SCK, MOSI signals of SPI1 to pins PA15, PA5, PA7, respectively
+    // set the alternate function bits for the pins
+    GPIOA->AFR[0] |= (0x0 << GPIO_AFRL_AFRL7_Pos) | (0x0 << GPIO_AFRL_AFRL5_Pos);
+    GPIOA->AFR[1] |= (0x0 << GPIO_AFRH_AFRH7_Pos);
+
+    // disable SPI1
+    SPI1->CR1 &= ~SPI_CR1_SPE;
+
+    // set baud rate to max possible
+    SPI1->CR1 |= SPI_CR1_BR;
+
+    // configure for 10-bit word size
+    SPI1->CR2 |= SPI_CR2_DS_3 | SPI_CR2_DS_0; // 0x1001 used for 10-bit word size
+    SPI1->CR2 &= ~(SPI_CR2_DS_2 | SPI_CR2_DS_1);
+
+    // configure the SPI channel to be in master configuration
+    SPI1->CR1 |= SPI_CR1_MSTR;
+
+    // set the SS ouptut enable pit and enable NSSP
+    SPI1->CR2 |= SPI_CR2_SSOE;
+    SPI1->CR2 |= SPI_CR2_NSSP;
+
+    // set the TXDMAEN to enable DMA transfers on transmit buffer empty
+    SPI1->CR2 |= SPI_CR2_TXDMAEN;
+
+    // enable the SPI channel
+    SPI1->CR1 |= SPI_CR1_SPE;
+}
+
+void spi_cmd(unsigned int data)
+{
+    // wait until spi1 TX is empty
+    while ((SPI1->SR & SPI_SR_TXE) == 0)
+        ;
+
+    // copy data to spi1 data register
+    SPI1->DR = data;
+}
+void spi_data(unsigned int data)
+{
+    // call spi_cmd with (data | 0x200)
+    unsigned int val = data | 0x200;
+    spi_cmd(val);
+}
+void spi1_init_oled()
+{
+    int millisecond = 1000000;
+    // wait 1ms with nano_wait
+    nano_wait(millisecond);
+
+    // call spi_cmd with 0x38 to do a function set
+    spi_cmd(0x38);
+
+    // call spi_cmd with 0x08 to turn off the display
+    spi_cmd(0x08);
+
+    // call spi_cmd with 0x01 to clear the display
+    spi_cmd(0x01);
+
+    // wait 2ms using nano_wait
+    nano_wait(2 * millisecond);
+
+    // call spi_cmd with 0x06 to set the entry mode
+    spi_cmd(0x06);
+
+    // call spi_cmd with 0x02 to move the cursor to the home position
+    spi_cmd(0x02);
+
+    // call spi_cmd with 0x0c to turn on the display
+    spi_cmd(0x0c);
+}
+void spi1_display1(const char *string)
+{
+    // move the cursor to the home position
+    spi_cmd(0x02);
+
+    // for each character in the string
+    // call spi_data with the character
+
+    // iterate over the string using string++
+    while (*string != '\0')
+    {
+        spi_data(*string);
+        string++;
+    }
+}
+
+void spi_clear()
+{
+    // move the cursor to the home position
+    spi_cmd(0x02);
+
+    // for each character in the string
+    // call spi_data with the character
+
+    // iterate over the string using string++
+    for (int i = 0; i < 16; i++)
+    {
+        spi_data(' ');
+    }
+
+    // clear the second row
+    spi_cmd(0xc0);
+    for (int i = 0; i < 16; i++)
+    {
+        spi_data(' ');
+    }
+}
+
+void spi1_display2(const char *string)
+{
+    // move the cursor to the second row
+    spi_cmd(0xc0);
+
+    // for each character in the string
+    // call spi_data with the character
+
+    // iterate until null terminator
+    while (*string != '\0')
+    {
+        spi_data(*string);
+        string++;
+    }
+}
+
 int main(void)
 {
     internal_clock();
+    // printf("Hello, World!\n");
+    init_gpio();
+    init_exti();
+    setup_tim14();
 
-    printf("Hello, World!\n");
+    init_spi1();
+    spi1_init_oled();
+    spi1_display1("Hello, World!");
 
-    // Comment this for-loop before you demo part 1!
-    // Uncomment this loop to see if "ECE 362" is displayed on LEDs.
-    // for (;;) {
-    //     asm("wfi");
-    // }
-    // End of for loop
-    // Have fun.
+    read_hcsr04();
+
+    while (1)
+        ;
 }
--- src/support.c
+++ src/support.c
@@ -1,11 +1,17 @@
 #include "stm32f0xx.h"
 #include <string.h> // for memmove()
-#include <stdio.h> // for memmove()
+#include <stdio.h>  // for memmove()
 
-void nano_wait(unsigned int n) {
-    asm(    "        mov r0,%0\n"
-            "repeat: sub r0,#83\n"
-            "        bgt repeat\n" : : "r"(n) : "r0", "cc");
+void nano_wait(unsigned int n)
+{
+    asm("        mov r0,%0\n"
+        "repeat: sub r0,#83\n"
+        "        bgt repeat\n" : : "r"(n) : "r0", "cc");
+}
+
+void micro_wait(unsigned int n)
+{
+    nano_wait(n * 1000);
 }
 
 const char font[] = {
@@ -44,8 +50,7 @@ const char font[] = {
     0x20, // 96: backquote
     // Lowercase
     0x5f, 0x7c, 0x58, 0x5e, 0x79, 0x71, 0x6f, 0x74, 0x10, 0x0e, 0x00, 0x30, 0x00,
-    0x54, 0x5c, 0x73, 0x7b, 0x50, 0x6d, 0x78, 0x1c, 0x00, 0x00, 0x00, 0x6e, 0x00
-};
+    0x54, 0x5c, 0x73, 0x7b, 0x50, 0x6d, 0x78, 0x1c, 0x00, 0x00, 0x00, 0x6e, 0x00};
 
 //=============================================================================
 // Part 1: 7-segment display update with DMA
@@ -53,18 +58,23 @@ const char font[] = {
 
 extern uint16_t msg[8];
 
-void set_digit_segments(int digit, char val) {
+void set_digit_segments(int digit, char val)
+{
     msg[digit] = (digit << 8) | val;
 }
 
 void print(const char str[])
 {
     const char *p = str;
-    for(int i=0; i<8; i++) {
-        if (*p == '\0') {
-            msg[i] = (i<<8);
-        } else {
-            msg[i] = (i<<8) | font[*p & 0x7f] | (*p & 0x80);
+    for (int i = 0; i < 8; i++)
+    {
+        if (*p == '\0')
+        {
+            msg[i] = (i << 8);
+        }
+        else
+        {
+            msg[i] = (i << 8) | font[*p & 0x7f] | (*p & 0x80);
             p++;
         }
     }
@@ -74,25 +84,31 @@ void printfloat(float f)
 {
     char buf[10];
     snprintf(buf, 10, "%f", f);
-    for(int i=1; i<10; i++) {
-        if (buf[i] == '.') {
+    for (int i = 1; i < 10; i++)
+    {
+        if (buf[i] == '.')
+        {
             // Combine the decimal point into the previous digit.
-            buf[i-1] |= 0x80;
-            memcpy(&buf[i], &buf[i+1], 10-i-1);
+            buf[i - 1] |= 0x80;
+            memcpy(&buf[i], &buf[i + 1], 10 - i - 1);
         }
     }
     print(buf);
 }
 
-void append_segments(char val) {
-    for (int i = 0; i < 7; i++) {
-        set_digit_segments(i, msg[i+1] & 0xff);
+void append_segments(char val)
+{
+    for (int i = 0; i < 7; i++)
+    {
+        set_digit_segments(i, msg[i + 1] & 0xff);
     }
     set_digit_segments(7, val);
 }
 
-void clear_display(void) {
-    for (int i = 0; i < 8; i++) {
+void clear_display(void)
+{
+    for (int i = 0; i < 8; i++)
+    {
         msg[i] = msg[i] & 0xff00;
     }
 }
@@ -103,18 +119,20 @@ void clear_display(void) {
 
 // 16 history bytes.  Each byte represents the last 8 samples of a button.
 uint8_t hist[16];
-char queue[2];  // A two-entry queue of button press/release events.
-int qin;        // Which queue entry is next for input
-int qout;       // Which queue entry is next for output
+char queue[2]; // A two-entry queue of button press/release events.
+int qin;       // Which queue entry is next for input
+int qout;      // Which queue entry is next for output
 
 const char keymap[] = "DCBA#9630852*741";
 
-void push_queue(int n) {
+void push_queue(int n)
+{
     queue[qin] = n;
     qin ^= 1;
 }
 
-char pop_queue() {
+char pop_queue()
+{
     char tmp = queue[qout];
     queue[qout] = 0;
     qout ^= 1;
@@ -124,12 +142,13 @@ char pop_queue() {
 void update_history(int c, int rows)
 {
     // We used to make students do this in assembly language.
-    for(int i = 0; i < 4; i++) {
-        hist[4*c+i] = (hist[4*c+i]<<1) + ((rows>>i)&1);
-        if (hist[4*c+i] == 0x01)
-            push_queue(0x80 | keymap[4*c+i]);
-        if (hist[4*c+i] == 0xfe)
-            push_queue(keymap[4*c+i]);
+    for (int i = 0; i < 4; i++)
+    {
+        hist[4 * c + i] = (hist[4 * c + i] << 1) + ((rows >> i) & 1);
+        if (hist[4 * c + i] == 0x01)
+            push_queue(0x80 | keymap[4 * c + i]);
+        if (hist[4 * c + i] == 0xfe)
+            push_queue(keymap[4 * c + i]);
     }
 }
 
@@ -143,18 +162,22 @@ int read_rows()
     return (~GPIOC->IDR) & 0xf;
 }
 
-char get_key_event(void) {
-    for(;;) {
-        asm volatile ("wfi");   // wait for an interrupt
-       if (queue[qout] != 0)
+char get_key_event(void)
+{
+    for (;;)
+    {
+        asm volatile("wfi"); // wait for an interrupt
+        if (queue[qout] != 0)
             break;
     }
     return pop_queue();
 }
 
-char get_keypress() {
+char get_keypress()
+{
     char event;
-    for(;;) {
+    for (;;)
+    {
         // Wait for every button event...
         event = get_key_event();
         // ...but ignore if it's a release.
@@ -167,7 +190,8 @@ char get_keypress() {
 void show_keys(void)
 {
     char buf[] = "        ";
-    for(;;) {
+    for (;;)
+    {
         char event = get_key_event();
         memmove(buf, &buf[1], 7);
         buf[7] = event;
@@ -190,13 +214,16 @@ float getfloat(void)
     int enter = 0;
     clear_display();
     set_digit_segments(7, font['0']);
-    while(!enter) {
+    while (!enter)
+    {
         int key = get_keypress();
-        if (digits == 8) {
+        if (digits == 8)
+        {
             if (key != '#')
                 continue;
         }
-        switch(key) {
+        switch (key)
+        {
         case '0':
             if (digits == 0)
                 continue;
@@ -209,7 +236,7 @@ float getfloat(void)
         case '7':
         case '8':
         case '9':
-            num = num*10 + key-'0';
+            num = num * 10 + key - '0';
             decimal <<= 1;
             digits += 1;
             if (digits == 1)
@@ -218,7 +245,8 @@ float getfloat(void)
                 append_segments(font[key]);
             break;
         case '*':
-            if (decimal == 0) {
+            if (decimal == 0)
+            {
                 decimal = 1;
                 dot();
             }
@@ -226,14 +254,16 @@ float getfloat(void)
         case '#':
             enter = 1;
             break;
-        default: continue; // ABCD
+        default:
+            continue; // ABCD
         }
     }
     float f = num;
-    while (decimal) {
+    while (decimal)
+    {
         decimal >>= 1;
         if (decimal)
-            f = f/10.0;
+            f = f / 10.0;
     }
     return f;
 }
@@ -247,81 +277,108 @@ void dialer(void)
 {
     clear_display();
     // North American dial tone is 350 Hz and 440 Hz together...
-    set_freq(0,350);
-    set_freq(1,440);
-    char dial[16] = { 0 };
+    set_freq(0, 350);
+    set_freq(1, 440);
+    char dial[16] = {0};
     int dialed = 0;
     int replay = 0;
     int replayed = 0;
     clear_display();
-    for(;;) {
+    for (;;)
+    {
         char event;
-        if (replay) {
-            if (replayed == dialed) {
+        if (replay)
+        {
+            if (replayed == dialed)
+            {
                 replay = 0;
                 continue;
             }
             event = dial[replayed++];
-        } else {
+        }
+        else
+        {
             event = get_key_event();
-            if (event == (0x80 | '#')) {
+            if (event == (0x80 | '#'))
+            {
                 clear_display();
                 replay = 1;
                 replayed = 0;
                 continue;
             }
-            if ((event & 0x80) == 0) {
-                set_freq(0,0);
-                set_freq(1,0);
+            if ((event & 0x80) == 0)
+            {
+                set_freq(0, 0);
+                set_freq(1, 0);
                 continue;
             }
             event &= 0x7f;
-            if (dialed < 16) {
+            if (dialed < 16)
+            {
                 dial[dialed] = event;
                 dialed++;
             }
         }
-        append_segments(font[event+0]);
-        switch(event) {
+        append_segments(font[event + 0]);
+        switch (event)
+        {
         case '1':
         case '2':
         case '3':
-        case 'A': set_freq(0,697); break;
+        case 'A':
+            set_freq(0, 697);
+            break;
         case '4':
         case '5':
         case '6':
-        case 'B': set_freq(0,770); break;
+        case 'B':
+            set_freq(0, 770);
+            break;
         case '7':
         case '8':
         case '9':
-        case 'C': set_freq(0,852); break;
+        case 'C':
+            set_freq(0, 852);
+            break;
         case '*':
         case '0':
         case '#':
-        case 'D': set_freq(0,941); break;
+        case 'D':
+            set_freq(0, 941);
+            break;
         }
-        switch(event) {
+        switch (event)
+        {
         case '1':
         case '4':
         case '7':
-        case '*': set_freq(1,1209); break;
+        case '*':
+            set_freq(1, 1209);
+            break;
         case '2':
         case '5':
         case '8':
-        case '0': set_freq(1,1336); break;
+        case '0':
+            set_freq(1, 1336);
+            break;
         case '3':
         case '6':
         case '9':
-        case '#': set_freq(1,1477); break;
+        case '#':
+            set_freq(1, 1477);
+            break;
         case 'A':
         case 'B':
         case 'C':
-        case 'D': set_freq(1,1633); break;
+        case 'D':
+            set_freq(1, 1633);
+            break;
         }
-        if (replay) {
+        if (replay)
+        {
             nano_wait(125000000);
-            set_freq(0,0);
-            set_freq(1,0);
+            set_freq(0, 0);
+            set_freq(1, 0);
             nano_wait(80000000);
         }
     }
